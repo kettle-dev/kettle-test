5105768cff4f435546571da5f48a6d32b4eb3eb5
Peter H. Boling <peter.boling@gmail.com>
2026-06-20 07:48:49 -0600

    🎨 Template bootstrap by kettle-jem v7.0.0

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,6 +10,10 @@ ENV["FLOSS_FUNDING_KETTLE__TEST"] = "Free-as-in-beer"
 #       The rescue LoadError handles that scenario.
 begin
   require "kettle-soup-cover"
+  if Kettle::Soup::Cover::DO_COV
+    require "simplecov"
+    SimpleCov.start
+  end
   require "simplecov" if Kettle::Soup::Cover::DO_COV # `.simplecov` is run here!
 rescue LoadError => error
   # check the error message and re-raise when unexpected
