9d84acd3aaa7aa93f212f32db9ad7a019fd48eaf
Peter H. Boling <peter.boling@gmail.com>
2026-07-26 02:29:47 -0600

    🎨 Template bootstrap by kettle-jem v7.1.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -42,6 +42,9 @@ Please file a bug if you notice a violation of semantic versioning.
 
 ### Fixed
 
+- kettle-jem-template-20260726-002 - Generated version files now document their
+  version namespace and constants, reducing warning-only YARD lint output.
+
 ### Security
 
 ## [2.0.15] - 2026-07-24
--- lib/kettle/test/version.rb
+++ lib/kettle/test/version.rb
@@ -2,9 +2,12 @@
 
 module Kettle
   module Test
+    # Version namespace for this gem.
     module Version
+      # Current gem version.
       VERSION = "2.0.16"
     end
+    # Current gem version exposed at the traditional constant location.
     VERSION = Version::VERSION # Traditional Constant Location
   end
 end
